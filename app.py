cdd6df39b69c9962bdd3f5768201521d1f1a8e81
viniciushrcs <vinciushrcs@gmail.com>
2023-04-21 12:06:25 -0300

    fix return pagina_inicial

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps - vFinal"
+    return "Hello World"
 
 if __name__ == '__main__':
     app.run()
